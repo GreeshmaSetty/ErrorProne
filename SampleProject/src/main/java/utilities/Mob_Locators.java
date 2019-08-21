a79493490c347fc5d2d52ddf4f98a4a53f81a0e8
GreeshmaSetty <51078997+GreeshmaSetty@users.noreply.github.com>
2019-08-21 14:02:34 +0530

    updated

--- SampleProject/src/main/java/utilities/Mob_Locators.java
+++ SampleProject/src/main/java/utilities/Mob_Locators.java
@@ -2,17 +2,16 @@ package utilities;
 
 public class Mob_Locators {
 
-	    public  String search = "//input[@aria-label='Search']";
-	    
-	    //public static String wiki = "//a[@href='https://en.wikipedia.org/wiki/";
-	    
-	    public  String wikipedia_Link = "//a[starts-with(@href,'https://en.m.wikipedia.org/wiki')]";
-	    
-	    public  String directed_By="//th[contains(text(),'Directed by')]/../td/a";
-	    
-	    public  String wiki_imdblink="//span[@id='External_links']/../following-sibling::ul/li/a[@title='IMDb']//preceding-sibling::a[@href]";
-	    
-	    public  String imdblink_directed_By="//div[@class='credit_summary_item']//h4[contains(text(),'Director')]//following-sibling::a";
-
+public  String search = "//input[@id='search']";
+	
+	public  String step_in_channel = "//div[@id='avatar']/yt-img-shadow//img[@id='img']";
+	
+	public String Video="//div[@id='tabsContainer']/div/paper-tab[2]/div";
+			//+ "//paper-tab[@role='tab']/div[contains(text(),'Videos')]";
+	public String VideoSettings = "//div[@id='movie_player']//button[@aria-label='Settings']";
+	
+public String QualityMenu = "//div[@role='menu']//div[@class='ytp-menuitem-label'][text()='Quality']";
+public String QualityList = "//div[@class='ytp-panel ytp-quality-menu']";
+public String upnext="//div[@id='items']//span[@id='video-title'][@title]";
 	    
 }
--- SampleProject/src/test/java/testCases/Step_IN_Mob.java
+++ SampleProject/src/test/java/testCases/Step_IN_Mob.java
@@ -1,5 +1,8 @@
 package testCases;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
 import java.util.HashMap;
 import java.util.List;
 import org.openqa.selenium.Keys;
@@ -19,7 +22,7 @@ public class Step_IN_Mob extends Global {
 	// public BrowserConfig config = new BrowserConfig();
 	public CommonMethods common = new CommonMethods();
 	public Keywords actions = new Keywords();
-	public UI_Locators ulocator = new UI_Locators();
+	public UI_Locators mlocator = new UI_Locators();
 	public APICommon api = new APICommon();
 
 	@Test(priority = 0)
@@ -28,15 +31,21 @@ public class Step_IN_Mob extends Global {
 	common.Launch("Mobile", "https://www.youtube.com/");
 
 		try {
-			actions.getWebElement(ulocator.search).sendKeys("step-inforum" + Keys.ENTER);
+			logger.logPass("Launch", "Step In Forum Launched");
+			actions.getWebElement(mlocator.search).sendKeys("step-inforum" + Keys.ENTER);
+			logger.logPass("Enter", "Step In Forum Entered");
+
+			logger.logPass("Step In Forum Launched", "Step In Forum Launched");
 			//System.out.println("Print serach");
-			actions.waitImplicit(ulocator.step_in_channel, 5);
-			actions.getWebElement(ulocator.step_in_channel).click();
-			actions.waitImplicit(ulocator.Video, 5);
-			actions.getWebElement(ulocator.Video).click();
+			actions.waitImplicit(mlocator.step_in_channel, 5);
+			actions.getWebElement(mlocator.step_in_channel).click();
+			actions.waitImplicit(mlocator.Video, 5);
+			actions.getWebElement(mlocator.Video).click();
 			System.out.println("Print video1");
-			actions.waitImplicit(null, 2);
+			actions.waitImplicit(null, 3);
 			String videoName = api.API_Get("http://54.169.34.162:5252/video");
+			logger.logPass("Get Video name via API", "VideoName : "+videoName);
+
 			System.out.println("VideoName : "+videoName);
 			WebElement findVideo = null;
 			int flag = 0;
@@ -52,27 +61,41 @@ public class Step_IN_Mob extends Global {
 					break;
 				//actions.waitForPageToLoad(30);
 			}while(findVideo== null);
+			logger.logPass("Screenshot Of Video", "Located video");
 			if(findVideo==null) {
-				System.out.println("Failed Video Name not found");
+				logger.logFail("Video Not Found in list");
 			}
 			actions.jsScrollToElement(findVideo);
 			actions.click(findVideo);
 			actions.waitForPageToLoad(10);
-			actions.mouseHover(ulocator.VideoSettings);
-			actions.waitImplicit(ulocator.VideoSettings, 5);
-			actions.click(ulocator.VideoSettings);
-			actions.click(ulocator.QualityMenu);
+			actions.mouseHover(mlocator.VideoSettings);
+			actions.waitImplicit(mlocator.VideoSettings, 5);
+			actions.click(mlocator.VideoSettings);
+			actions.click(mlocator.QualityMenu);
 			actions.waitImplicit(null, 1);
-			//String listQuality = actions.getWebElement(ulocator.QualityList).getText();
+			//String listQuality = actions.getWebElement(mlocator.QualityList).getText();
 			actions.select("360");			
-			List<WebElement>videolist=actions.getWebElementList(ulocator.upnext);
+			List<WebElement>videolist=actions.getWebElementList(mlocator.upnext);
 			String[] upNextList = new String[videolist.size()];
 			for(int j=0; j<videolist.size();j++)
 	        {
 				upNextList[j]=videolist.get(j).getAttribute("title");
 	            System.out.println(upNextList[j]);
 	        }
-			actions.jsonCreate(upNextList);
+			String jsonValue = actions.jsonCreate(upNextList);
+			File file = new File(System.getProperty("user.dir") + "//src//main//resources//jsonBody.json");
+			Writer writer = new FileWriter(file,true);
+			writer.write(jsonValue);
+			writer.close();
+			
+
+			String response = api.API_Post(file, "http://54.169.34.162:5252/upload");
+			logger.logPass("API uploaded response",response);
+			
+			String getResponse = api.API_Get("http://54.169.34.162:5252/result/"+response);
+			logger.logPass("API Get response",getResponse);
+
+			
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
